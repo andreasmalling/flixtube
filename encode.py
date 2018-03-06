058cef2e03a53a485ab3976aec31a4b9a2eb4c39
Andreas Malling Østergaard <andreas.malling@gmail.com>
2018-03-06 11:19:54 +0100

    so it begins

--- encode.py
+++ encode.py
@@ -1,4 +1,24 @@
 # Translate encode.sh to python
+import sys
+import os
+
+if (len(sys.argv) < 2):
+    print("Please provide segment duration and input file as arguments")
+    sys.exit(1)
+
+SEGDURATION = int(sys.argv[1])
+INPUTFILE = sys.argv[2]
+
+FRAMERATE = 25
+
+KEYFRAMERATE = (SEGDURATION * FRAMERATE) / 1000
+
+FILENAME = os.path.splitext(INPUTFILE)[0]
+ENCODING = FILENAME + "_key_" + KEYFRAMERATE + ".mp4"
+MPD = str(SEGDURATION) + FILENAME
+SEGS = MPD + "_$RepresentationID$/Segment_$Number$$Init=0$"
+
+print("mpd: " + MPD)
 
 
 # 1) Add video to IPFS
