ee32de7242e7309cb3a6464977e9231a06937366
kgoyo <kgoyo666@gmail.com>
2018-03-06 11:51:46 +0100

    upload to ipfs

--- encode.py
+++ encode.py
@@ -1,6 +1,9 @@
 # Translate encode.sh to python
 import sys
 import os
+import subprocess
+import re
+from xml.dom import minidom
 
 if (len(sys.argv) < 2):
     print("Please provide segment duration and input file as arguments")
@@ -22,6 +25,27 @@ print("mpd: " + MPD)
 
 
 # 1) Add video to IPFS
+
+video_folder_name = FILENAME + "_" + str(SEGDURATION) + "_video"
+audio_folder_name = FILENAME + "_" + str(SEGDURATION) + "_audio"
+
+
+# upload video
+process = subprocess.Popen(["ipfs", "add", "-r", video_folder_name], stdout=subprocess.PIPE)
+out, err = process.communicate()
+m = re.search("added (.+?) " + video_folder_name + "\n", out.decode("utf-8"))
+if m:
+    video_hash = m.group(1)
+
+#upload audio
+process = subprocess.Popen(["ipfs", "add", "-r", audio_folder_name], stdout=subprocess.PIPE)
+out, err = process.communicate()
+m = re.search("added (.+?) " + audio_folder_name + "\n", out.decode("utf-8"))
+if m:
+    audio_hash = m.group(1)
+
+print("video hash:", video_hash, "audio hash:", audio_hash)
+
 # 2) Edit mpd
 # 3) Add mpd to IPFS
 
