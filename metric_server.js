62da97534db2c650894239721f645b3663988fd2
Andreas Malling Østergaard <andreas.malling@gmail.com>
2018-03-07 10:38:32 +0100

    begin work on metric_server

---
+++ metric_server.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const ipfsAPI = require("ipfs-api");
+const ipfs = ipfsAPI("localhost", "5001", {protocol: "http"});
+
+var path;
+
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const app = express();
+
+// default options
+app.use(fileUpload());
+
+app.get("/", function (ignore, res) {   // ignore = req
+    res.writeHead(200, {"Content-Type": "text/html"});
+    res.write("<form ref=\"uploadForm\" id=\"uploadForm\" action=\"upload\" method=\"post\" encType=\"multipart/form-data\">");
+    res.write("<input type=\"file\" name=\"sampleFile\"><br>");
+    res.write("<input type=\"submit\">");
+    res.write("</form>");
+    return res.end();
+});
+
+app.post("/upload", function (req, res, ignore) {   // ignore = next (?)
+    if (!req.files) {
+        return res.status(400).send("No files were uploaded.");
+    }
+
+    ipfs.add(req.files.sampleFile.data, function (err, files) {
+        if (err) {
+            console.log(err);
+        }
+
+        console.log(files);
+        files.forEach(function (file) {
+            path = file.path;
+            console.log(path);
+        });
+        res.send("File uploaded! Available at <a href=\"http://ipfs.io/ipfs/" + path + "\">" + path + "</a>");
+    });
+
+});
+
+app.post("/metrics", function (req, res, ignore) {   // ignore = next (?)
+});
+
+app.listen(8081);
