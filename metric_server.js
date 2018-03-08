0f5206b9ace24bd9e9a3d471346669b6f090ff68
kgoyo <kgoyo666@gmail.com>
2018-03-08 10:46:30 +0100

    mongo

--- metric_server.js
+++ metric_server.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 const ipfsAPI = require("ipfs-api");
 const ipfs = ipfsAPI("localhost", "5001", {protocol: "http"});
@@ -7,9 +7,9 @@ var path;
 
 const express = require("express");
 const fileUpload = require("express-fileupload");
-const bodyParser = require('body-parser');
-var MongoClient = require('mongodb').MongoClient;
-var mongo_url = "mongodb://localhost:27017/mydb";
+const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
+const mongo_url = "mongodb://localhost:27017/flixtube_db";
 const app = express();
 
 // default options
@@ -46,14 +46,73 @@ app.post("/upload", function (req, res, ignore) {   // ignore = next (?)
 
 });
 
+//mongo db vars
+var database = null;
+var flixtube_db = null;
+
+
 app.post("/metrics", function (req, res, ignore) {   // ignore = next (?)
-    console.log(req.body);
+    var json = req.body;
+    console.log(json);
+    if (flixtube_db !== null) {
+        //do db things
+        flixtube_db.collection("metrics").insertOne(json, function (err, ignore) { // ignore res
+            if (err) {
+                throw err;
+            }
+            console.log("inserted document in db");
+        });
+    }
     res.send(req.body);
 });
 
 app.listen(8081);
-MongoClient.connect(mongo_url, function(err, db) {
-    if (err) throw err;
-    console.log("Database created!");
-    db.close();
+
+// create mongo db collection instance
+
+MongoClient.connect(mongo_url, function (err, db) {
+    if (err) {
+        throw err;
+    }
+    database = db;
+    var dbo = db.db("flixtube_db");
+    flixtube_db = dbo;
+    dbo.createCollection("metrics", function (err, ignore) { // ignore res
+        if (err) {
+            throw err;
+        }
+        console.log("collection created!");
+    });
 });
+
+
+// handle close
+process.stdin.resume();//so the program will not close instantly
+
+function exitHandler(options, err) {
+    if (options.cleanup) {
+        if (database !== null) {
+            database.close();
+        }
+        console.log("closing db");
+    }
+    if (err) {
+        console.log(err.stack);
+    }
+    if (options.exit) {
+        process.exit();
+    }
+}
+
+//do something when app is closing
+process.on("exit", exitHandler.bind(null, {cleanup: true}));
+
+//catches ctrl+c event
+process.on("SIGINT", exitHandler.bind(null, {exit: true}));
+
+// catches "kill pid" (for example: nodemon restart)
+process.on("SIGUSR1", exitHandler.bind(null, {exit: true}));
+process.on("SIGUSR2", exitHandler.bind(null, {exit: true}));
+
+//catches uncaught exceptions
+process.on("uncaughtException", exitHandler.bind(null, {exit: true}));
