769b8102df5559a2e1fd86f1d1e6ce35e8e5da8c
kgoyo <kgoyo666@gmail.com>
2018-03-21 13:05:55 +0100

    plot generates .csv file

--- plot/plot.py
+++ plot/plot.py
@@ -11,15 +11,20 @@ def plotFromCollection(collectionName, x, y):
     data.sort(key=lambda x: x[0])  # sort based on first element of tuble
     xlist, ylist = map(list, zip(*data))  # split list of tubles into 2 lists
     plt.xlim(xmin=xlist[0], xmax=xlist[-1])
-    print(xlist)
-    print(xlist[-1])
-    plt.plot(xlist, ylist) #change this to the kind of plot you want
+    plt.plot(xlist, ylist) # change this to the kind of plot you want
     plt.title(collectionName)
     plt.show()
+
+    with open("plot.csv", "w") as file:
+        file.write(x + " " + y + "\n")
+        for i in range(0, len(xlist)):
+            file.write(str(xlist[i]) + " " + str(ylist[i]) + "\n")
+        file.close()
+
 
 db = client["flixtube_db"]
 
 
-plotFromCollection("video", "timestamp", "latency")
+plotFromCollection("video", "seg", "latency")
 
 client.close()
