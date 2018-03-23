2d92969e41a0cdf4f3dada082804bcbf72797544
Andreas Malling Østergaard <andreas.malling@gmail.com>
2018-03-23 13:06:46 +0100

    Updates error handling, only IPFS hashing and moves dashed file to new path for easier mounting

--- encode.py
+++ encode.py
@@ -1,12 +1,12 @@
 # Translate encode.sh to python
 import sys
 import os
-import re
+from shutil import rmtree, move
 from xml.dom import minidom
 import subprocess
 from ffprobe3 import FFProbe
 
-print("v0.5\n")
+print("v0.7\n")
 
 if (len(sys.argv) < 2):
     print("Please provide segment duration and input file as arguments")
@@ -21,26 +21,46 @@ for stream in metadata.streams:
     if stream.is_video():
         FRAMERATE = stream.frames() / stream.duration_seconds()
 
-KEYFRAMERATE = (SEGDURATION * FRAMERATE) / 1000
+KEYFRAMERATE = round((SEGDURATION * FRAMERATE) / 1000)
+
+DIR_DASHED = "video_dashed"
+DIR_ENCODED = "video_encoded"
 
 FILENAME = os.path.splitext(os.path.basename(INPUTFILE))[0]
-ENCODING = FILENAME + "_key_" + str(KEYFRAMERATE) + ".mp4"
+FILE_ENCODED = DIR_ENCODED + "/" + FILENAME + "_key_" + str(KEYFRAMERATE) + ".mp4"
 MPD = FILENAME + "_" + str(SEGDURATION)
 SEGS = MPD + "_$RepresentationID$/Segment_$Number$$Init=0$"
 
+# Set up folders if first run
+def makeFolder(folderpath):
+    if not os.path.exists(folderpath):
+        os.makedirs(folderpath)
+
+dashed = os.path.join(os.getcwd(), DIR_DASHED)
+encoded = os.path.join(os.getcwd(), DIR_ENCODED)
+
+makeFolder(dashed)
+makeFolder(encoded)
+
 # Clean up previous runs with same parameters
-subprocess.run(["rm", "-r", MPD + "*"])
+for fname in os.listdir(dashed):
+    if fname.startswith(MPD):
+        path = os.path.join(dashed, fname)
+        if os.path.isfile(path):
+            os.remove(path)
+        else:
+            rmtree(path)
 
 print("Calculated KEYFRAMERATE:", KEYFRAMERATE)
 
 # (Re-)encode (https://stackoverflow.com/a/30982414)
 subprocess.run(["ffmpeg",
-				"-loglevel", "error",
+                "-loglevel", "error",
                 "-i", INPUTFILE,
                 "-c:v", "libx264",
                 "-x264-params", "keyint=" + str(KEYFRAMERATE) + ":scenecut=0",
                 "-strict", "-2", "-c:a", "aac",
-                ENCODING])
+                FILE_ENCODED])
 
 # Notes on encoding:
 # ==================
@@ -61,37 +81,32 @@ subprocess.run(["MP4Box",
                 "-segment-name", SEGS,
                 "-bs-switching", "no",
                 "-url-template",
-                ENCODING + "#video:id=video",
-                ENCODING + "#audio:id=audio"])
+                FILE_ENCODED + "#video:id=video",
+                FILE_ENCODED + "#audio:id=audio"])
 
 # 1) Add video to IPFS
 
-video_folder_name = FILENAME + "_" + str(SEGDURATION) + "_video"
-audio_folder_name = FILENAME + "_" + str(SEGDURATION) + "_audio"
+video_folder_name = MPD + "_video"
+audio_folder_name = MPD + "_audio"
 
 if not(os.path.exists(video_folder_name) and os.path.exists(audio_folder_name) and os.path.exists(MPD + ".mpd")):
-	print("Files not generated for upload to IPFS")
-	sys.exit(2)
+    print("Files not generated for upload to IPFS")
+    sys.exit(2)
 
-# upload video
-process = subprocess.Popen(["ipfs", "add", "-r", video_folder_name], stdout=subprocess.PIPE)
-out, err = process.communicate()
-m = re.search("added (.+?) " + video_folder_name + "\n", out.decode("utf-8"))
-if m:
-    video_hash = m.group(1)
 
-#upload audio
-process = subprocess.Popen(["ipfs", "add", "-r", audio_folder_name], stdout=subprocess.PIPE)
-out, err = process.communicate()
-m = re.search("added (.+?) " + audio_folder_name + "\n", out.decode("utf-8"))
-if m:
-    audio_hash = m.group(1)
+def ipfs_hash(folder_name):
+    process = subprocess.Popen(["ipfs", "add", "-r", "-Q", "--only-hash", folder_name], stdout=subprocess.PIPE)
+    out, err = process.communicate()
+    return out.decode("utf-8")
+
+audio_hash = ipfs_hash(audio_folder_name)
+video_hash = ipfs_hash(video_folder_name)
 
 # 2) Edit mpd
 
-if not('audio_hash' in globals() and 'vidleo_hash' in globals()):
-    print("Files not uploaded to IPFS")
-    sys.exit(3)
+if audio_hash is None or video_hash is None:
+    print("Files not hashed for IPFS")
+    sys.exit(1)
 
 mpd = minidom.parse(MPD + ".mpd")
 adaptionSets = mpd.getElementsByTagName("AdaptationSet")
@@ -103,11 +118,16 @@ for a in adaptionSets:
         a.getElementsByTagName("SegmentTemplate")[0].setAttribute("media",          audio_hash + "/Segment_$Number$.m4s")
         a.getElementsByTagName("SegmentTemplate")[0].setAttribute("initialization", audio_hash + "/Segment_0.mp4")
 # write to file
-f = open(MPD + "_fixed.mpd", "w")
+f = open(MPD + ".mpd", "w")
 mpd.writexml(f)
 f.close()
 
 
 # 3) Add mpd to IPFS
 print("")
-subprocess.call(["ipfs", "add", MPD + "_fixed.mpd"])
+subprocess.call(["ipfs", "add", "-Q", "--only-hash", MPD + ".mpd"])
+
+# Move newly created files
+move(os.path.join(os.getcwd(), video_folder_name), dashed)
+move(os.path.join(os.getcwd(), audio_folder_name), dashed)
+move(os.path.join(os.getcwd(), MPD + ".mpd"), dashed)
