5c8086eed603f66073a9ae427a8cbc4cfc6f3174
kgoyo <kgoyo666@gmail.com>
2018-05-02 11:19:00 +0200

    adds role based plots

--- plot/plot.py
+++ plot/plot.py
@@ -16,11 +16,11 @@ WAIT = "wait"
 PERSONA = "persona"
 NETWORK = "network"
 
-
+BEHAVIOURS = ["BINGE", "INCOGNITO", "SKIPPER", "IDLE", "LEECHER"]
 
 # init mongo client
-# client = pymongo.MongoClient("localhost", 27017) #temporary ip for testing
-client = pymongo.MongoClient("mongo", 27017)
+client = pymongo.MongoClient("localhost", 27017) #temporary ip for testing
+# client = pymongo.MongoClient("mongo", 27017)
 db = client["flixtube_db"]
 
 
@@ -31,30 +31,46 @@ def main():
     os.mkdir(PATH)
     users = [persona for persona in db[PERSONA].find()]
     users_no_idle = list(filter(lambda x: x["type"] != "IDLE", users))
+
     # give users identifying number
     for i in range(len(users)):
         users[i]["num"] = i
-    print("plotting user data for segments")
-    plot_user_data_seg("latency", users_no_idle, AUDIO)
-    plot_user_data_seg("download", users_no_idle, AUDIO)
-    plot_user_data_seg("latency", users_no_idle, VIDEO)
-    plot_user_data_seg("download", users_no_idle, VIDEO)
 
-    print("plotting user data over time")
-    plot_user_data_time("latency", users_no_idle, AUDIO)
-    plot_user_data_time("download", users_no_idle, AUDIO)
-    plot_user_data_time("latency", users_no_idle, VIDEO)
-    plot_user_data_time("download", users_no_idle, VIDEO)
+    #behaviour average plots
+    users_b = []
+    for i in range(len(BEHAVIOURS)):
+        tmp = list(filter(lambda x: x["type"] == BEHAVIOURS[i], users))
+        if tmp:
+            users_b += [tmp]
 
-    print("plotting network data over time")
-    plot_network_data_time("rx_bytes", users)
-    plot_network_data_time("tx_bytes", users)
-    plot_network_data_time("rx_packets", users)
-    plot_network_data_time("tx_packets", users)
 
-    print("plotting network histogram")
-    plot_network_data_hist("rx_bytes", "tx_bytes", users)
-    plot_network_data_hist("rx_packets", "tx_packets", users)
+    # print("plotting user data for segments")
+    # plot_user_data_seg("latency", users_no_idle, AUDIO)
+    # plot_user_data_seg("download", users_no_idle, AUDIO)
+    # plot_user_data_seg("latency", users_no_idle, VIDEO)
+    # plot_user_data_seg("download", users_no_idle, VIDEO)
+    #
+    # print("plotting user data over time")
+    # plot_user_data_time("latency", users_no_idle, AUDIO)
+    # plot_user_data_time("download", users_no_idle, AUDIO)
+    # plot_user_data_time("latency", users_no_idle, VIDEO)
+    # plot_user_data_time("download", users_no_idle, VIDEO)
+    #
+    # print("plotting network data over time")
+    # plot_network_data_time("rx_bytes", users)
+    # plot_network_data_time("tx_bytes", users)
+    # plot_network_data_time("rx_packets", users)
+    # plot_network_data_time("tx_packets", users)
+    #
+    # print("plotting network histogram")
+    # plot_network_data_hist("rx_bytes", "tx_bytes", users)
+    # plot_network_data_hist("rx_packets", "tx_packets", users)
+
+
+    plot_user_data_seg_role("latency", users_b, AUDIO)
+    plot_user_data_seg_role("download", users_b, AUDIO)
+    plot_user_data_seg_role("latency", users_b, VIDEO)
+    plot_user_data_seg_role("download", users_b, VIDEO)
 
     # close mongo client
     client.close()
@@ -88,6 +104,37 @@ def plot_user_data_seg(yname, users, collection):
     plt.ylabel(yname)
 
     path = PATH + collection + "_" + yname + "_seg"
+    csv.export(path + ".csv")
+    plt.savefig(path + ".png", bbox_inches='tight')
+    plt.gcf().clear()
+
+
+def plot_user_data_seg_role(yname, users_b, collection):
+    csv = CSVBuilder()
+    last_seg = db[AUDIO].find_one(sort=[("seg", pymongo.DESCENDING)])["seg"]
+    segments = [x for x in range(last_seg+1)]
+    csv.add_plot("segments", segments)
+    for role in users_b:
+        for user in role:
+            print(user["type"])
+            user[yname] = [0 for i in range(last_seg+1)] #maybe change 0 to something else?????????
+            for res in db[collection].find({"ip": user["ip"], "responsecode": 200}).sort("seg", pymongo.ASCENDING):
+                if res["seg"] < len(user[yname]):
+                    user[yname][res["seg"]] = res[yname]
+        means = [statistics.mean([user[yname][i] for user in role]) for i in range(last_seg+1)]
+        plt.plot(segments, means, label="mean:"+role[0]["type"])
+        csv.add_plot("mean:"+role[0]["type"], means)
+
+        stdev = [statistics.stdev([user[yname][i] for user in role]) for i in range(last_seg+1)]
+        plt.plot(segments, stdev, color="blue", label="stdev:"+role[0]["type"])
+        csv.add_plot("stdev:"+role[0]["type"], stdev)
+
+    plt.title(collection + " " + yname + " per segments")
+    plt.legend()
+    plt.xlabel("segment number")
+    plt.ylabel(yname)
+
+    path = PATH + collection + "_" + yname + "_seg_role"
     csv.export(path + ".csv")
     plt.savefig(path + ".png", bbox_inches='tight')
     plt.gcf().clear()
