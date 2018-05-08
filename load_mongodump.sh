0db8c98f6da0efcd656ef8f0985f8eaed49cf8b6
Andreas Malling Østergaard <andreas.malling@gmail.com>
2018-05-08 10:24:44 +0200

    Adds options to load script

--- load_mongodump.sh
+++ load_mongodump.sh
@@ -1,14 +1,30 @@
 #! /bin/bash
 
-if [ $# -eq 0 ]; then
-	FILE=$(ls -t ./data/dump | head -1)
-else
-	FILE=$(basename $1)
-fi
+DOCKER='true'
+FILE=$(ls -t ./data/dump | head -1)
+
+while getopts 'hlf:' flag; do
+  case "${flag}" in
+    h) echo "Help:"; echo -e "  -l\t Run locally"; echo -e "  -f\t Use alt file dump \n\t (Default: newest in data/dump)" ; exit 0 ;;
+    l) DOCKER='false' ;;
+    f) FILE="$(basename ${OPTARG})" ;;
+    *) error "Unexpected option ${flag}" ;;
+  esac
+done
 
 echo "Loading: $FILE"
 echo ""
 
-docker-compose -f plot-compose.yml up -d --scale plot=0
-docker exec flixtube_mongo_1 mongorestore --drop --gzip --db flixtube_db --archive=/data/dump/$FILE
-docker exec -ti flixtube_mongo_1 mongo flixtube_db
+if $DOCKER; then
+	echo "Running in Docker"
+    docker-compose -f plot-compose.yml up -d --scale plot=0
+    docker exec flixtube_mongo_1 mongorestore --drop --gzip --db flixtube_db --archive=/data/dump/$FILE
+    docker exec -ti flixtube_mongo_1 mongo flixtube_db
+else
+	echo "Running Locally"
+	mongorestore --drop --gzip --db flixtube_db --archive="$PWD/data/dump/$FILE"
+	mongo flixtube_db
+fi
+
+
+
