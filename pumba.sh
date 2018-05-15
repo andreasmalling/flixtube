08ede774aa84ba61deff5748d63087a8df29cfe6
Andreas Malling Østergaard <andreas.malling@gmail.com>
2018-05-15 09:34:00 +0200

    Updates pumba.sh

--- pumba.sh
+++ pumba.sh
@@ -2,34 +2,40 @@
 USERS='re2:user_[1-6]_[0-9]*'
 ALL_USERS='re2:user_([1-6]|seed|debug)_[0-9]*'
 SEEDS="$ALL_USERS"
+TCIMG=gaiadocker/iproute2
+DURATION=10m
 
-# docker pull gaiadocker/iproute2
+
+# Chech for tc image
+if [[ "$(docker images -q $TCIMG 2> /dev/null)" == "" ]]; then
+	printf "Missing image: $TCIMG\n"
+	docker pull $TCIMG
+fi
+
 
 slow_network() {
 	echo Slow network: $1
-	pumba netem --tc-image gaiadocker/iproute2 --duration 10m rate --rate $2 $1 &
+	pumba netem --tc-image $TCIMG --duration $DURATION rate --rate $2 $1 &
 	RATE_PID=$!
 	printf "Slow PID:\t$RATE_PID\n"
 }
 
+
 late_network() {
 	echo Late network: $1
-	pumba netem --tc-image gaiadocker/iproute2 --duration 10m delay --time 40 --jitter 10 $1 &
+	pumba netem --tc-image $TCIMG --duration $DURATION delay --time 40 --jitter 10 $1 &
 	DELAY_PID=$!
 	printf "Late PID:\t$DELAY_PID\n"
 }
 
+
 random_pause() {
 	echo Random pause: $1
-	pumba --random --interval 1s pause --duration 2s $1 &
+	pumba --random --interval 10s pause --duration $DURATION $1 &
+	PAUSE_PID=$!
+	printf "Pause PID:\t$DELAY_PID\n"
 }
 
-handler()
-{
-    printf "\nKilling: $RATE_PID\n"
-    kill -s SIGINT $RATE_PID
-    exit 0
-}
 
 kill_pumba()
 {
